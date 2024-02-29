2377146d939c4c390f8c494c6599ed01265b4083
nisargpatel1504 <nisargpatel1504@gmail.com>
2024-02-28 19:49:44 -0500

    Declared varaible for rate limitng seconds

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// src/config/index.js
+const TWO_SECOND_RATE_LIMIT_TIME = 2000;
 
 export const metaApi = {
   accessToken: process.env.FACEBOOK_ACCESS_TOKEN,
   baseUrl: "https://graph.facebook.com/v19.0",
-  rateLimitWindowMs: 2000, // Example: 2 seconds window for rate limiting
+  rateLimitWindowMs: TWO_SECOND_RATE_LIMIT_TIME, // Example: 2 seconds window for rate limiting
 };
 
 export const logLevel = process.env.LOG_LEVEL || "info";
