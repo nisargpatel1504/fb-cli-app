fb96859ad5b6bf4ea6015e791cc39338f3b38240
nisargpatel1504 <nisargpatel1504@gmail.com>
2024-02-28 19:56:16 -0500

    Modified config file by adding rate limit second variable

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
-
-// src/config/index.js
-
+const RATE_LIMIT_SECONDS = 2000; //2000 means 2 seconds
 export const metaApi = {
   accessToken: process.env.FACEBOOK_ACCESS_TOKEN,
   baseUrl: "https://graph.facebook.com/v19.0",
-  rateLimitWindowMs: 2000, // Example: 2 seconds window for rate limiting
+  rateLimitWindowMs: RATE_LIMIT_SECONDS,
 };
 
 export const logLevel = process.env.LOG_LEVEL || "info";
